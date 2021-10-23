8db0a1839f46d2cd73e863368583a584f5eaeedd
Rully Wijiantoko <palmbiscuit08@gmail.com>
2021-10-23 14:45:55 +0700

    first init

---
+++ app/src/main/java/com/rully/loginapp/view/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rully.loginapp.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rully.loginapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
